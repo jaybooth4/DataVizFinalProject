874a8b0255f06a355396a3d203a92b3b19f3fe02
Jason Booth <booth.j@husky.neu.edu>
2019-03-21 12:16:32 -0400

    Added file to download data from a google storage bucket

--- writeToBucket.py
+++ writeToBucket.py
@@ -1,12 +1,8 @@
-from google.cloud import storage
 from google.cloud import bigquery
 
 def extractTable(project, datasetID, tableID, bucket):
     """ Creates an export job from BigQuery to Cloud storage """
 
-    # project = 'bigquery-public-data'
-    # dataset_id = 'samples'
-    # table_id = 'shakespeare'
     client = bigquery.Client()
 
     destinationURI = 'gs://{}/{}'.format(bucket, tableID)
@@ -16,26 +12,12 @@ def extractTable(project, datasetID, tableID, bucket):
     extract_job = client.extract_table(
         table_ref,
         destinationURI,
-        # Location must match that of the source table.
-        location='US')  # API request
-    extract_job.result()  # Waits for job to complete.
+        location='US') 
+
+    # Waits for job to complete.
+    extract_job.result()  
 
     print('Exported {}:{}.{} to {}'.format(
         project, datasetID, tableID, destinationURI))
 
-def downloadBlob(bucketName, sourceBlobName, destinationFileName):
-    """Downloads a blob from the bucket."""
-
-    storage_client = storage.Client()
-    bucket = storage_client.get_bucket(bucketName)
-    blob = bucket.blob(sourceBlobName)
-
-    blob.download_to_filename(destinationFileName)
-
-    print('Blob {} downloaded to {}.'.format(
-        sourceBlobName,
-        destinationFileName))
-
 extractTable("bigquery-public-data", "ncaa_basketball", "mascots", "datavizfinal")
-
-# downloadBlob("datavizfinal", "test.txt", "test2.txt")
