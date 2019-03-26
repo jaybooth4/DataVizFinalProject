8ac63d59580437e20b5b7a03d6007abe7514e250
Jason Booth <booth.j@husky.neu.edu>
2019-03-26 09:46:57 -0400

    Integrated model file with custom dataset

--- RNN/RNNLSTM-model.py
+++ RNN/RNNLSTM-model.py
@@ -2,20 +2,16 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from .readData import RNNDataset
 
 torch.manual_seed(1)
 
 def main():
-    # Define transforms
-    transformations = transforms.Compose([])
     # Define custom dataset
-    custom_mnist_from_csv = \
-        CustomDatasetFromCSV('../data/mnist_in_csv.csv',
-                             28, 28,
-                             transformations)
+    playData = RNNDataset('data/play.csv')
     # Define data loader
-    mn_dataset_loader = torch.utils.data.DataLoader(dataset=custom_mnist_from_csv,
-                                                    batch_size=10,
+    mn_dataset_loader = torch.utils.data.DataLoader(dataset=playData,
+                                                    batch_size=2,
                                                     shuffle=False)
     
     for images, labels in mn_dataset_loader:
