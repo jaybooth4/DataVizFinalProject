e3b87353cdd656e9949c53346336f48880a6132e
Jason Booth <booth.j@husky.neu.edu>
2019-04-04 17:56:36 -0400

    Added final preprocessing file for dataset

--- cloud/pipeline.py
+++ cloud/pipeline.py
@@ -2,7 +2,7 @@ from queryAndSaveResultToTable import createNewTableFromQuery
 from writeToBucket import extractTable
 from downloadData import downloadBlob
 
-queryPbpSub = "SELECT game_id, game_clock, elapsed_time_sec, team_basket, event_id, event_description, event_coord_x, event_coord_y, event_type, type, shot_made, shot_type, shot_subtype, three_point_shot, points_scored, turnover_type, rebound_type, timeout_duration FROM `bigquery-public-data.ncaa_basketball.mbb_pbp_sr` WHERE event_coord_x IS NOT NULL AND event_coord_y IS NOT NULL ORDER BY game_id, elapsed_time_sec;"
+queryPbpSub = "SELECT game_id, elapsed_time_sec, team_basket, event_id, event_description, event_coord_x, event_coord_y, event_type, type, shot_made, shot_type, shot_subtype, three_point_shot, points_scored, turnover_type, rebound_type, timeout_duration FROM `bigquery-public-data.ncaa_basketball.mbb_pbp_sr` WHERE event_coord_x IS NOT NULL AND event_coord_y IS NOT NULL ORDER BY game_id, elapsed_time_sec;"
 
 
 def main():
---
+++ preprocess/FinalFieldsPBP.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+IN_FILE_NAME = "../data/pbp_from_query_ordered_small.csv"
+OUT_FILE_NAME = "../data/pbp_preprocessed_small.csv"
+DROP_COLS = ['turnover_type', 'rebound_type', 'timeout_duration', 'event_id', 'event_description', 'shot_type', 'shot_subtype', 'type', 'shot_made', 'three_point_shot']
+CAT_VARS = ['team_basket', 'event_type']
+FILL_NA = 'points_scored'
+
+
+def dropCols(pbpDF, dropCols):
+    # Drops unneeded columns
+    return pbpDF.drop(["Unnamed: 0"] + dropCols, 1)
+
+def fillNA(pbpDF, col, value):
+    # Fill a column with a given value
+    pbpDF[FILL_NA] = pbpDF[FILL_NA].fillna(value)
+    return pbpDF
+
+def encodeCategorical(df, catColumns):
+    # Encodes categorical variables
+    for var in catColumns:
+        catDf = pd.get_dummies(df[var], prefix=var)
+        df=df.join(catDf)
+    df.drop(catColumns, 1, inplace=True)
+    return df
+
+def preprocessPBPData(fileName):
+    # Preprocesses the dataset
+    pbpDF = pd.read_csv(fileName)
+    pbpDF = dropCols(pbpDF, DROP_COLS)
+    pbpDF = fillNA(pbpDF, FILL_NA, 0.0)
+    pbpDF = encodeCategorical(pbpDF, CAT_VARS)
+    return pbpDF
+
+def main():
+    preprocessedData = preprocessPBPData(IN_FILE_NAME)
+    preprocessedData.to_csv(OUT_FILE_NAME, index=False)
+
+if __name__ == '__main__':
+    main()
