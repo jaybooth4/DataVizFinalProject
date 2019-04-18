36b0740303d8072db6c8f0866b0396b9fe70a296
Joseph Mulhern <guitarman513@gmail.com>
2019-04-18 11:28:13 -0400

    fixed typo in pipeline

--- SVM_LogReg_DecTree_Pipeline/pipeline.py
+++ SVM_LogReg_DecTree_Pipeline/pipeline.py
@@ -24,21 +24,19 @@ from HelperFiles.Visualizations import getHeatMap, getROC, getTSNE
 
 
 def main():
-    #createNewTableFromQuery(playerQuery, "concrete-fabric-234819", "player_data_table", "PlayerData")
-    #extractTable("concrete-fabric-234819", "player_data_table", "PlayerData", "playerquery")
-    #downloadBlob("playerquery", "PlayerData-000000000000", "PlayerData.csv")
+    createNewTableFromQuery(playerQuery, "concrete-fabric-234819", "player_data_table", "PlayerData")
+    extractTable("concrete-fabric-234819", "player_data_table", "PlayerData", "playerquery")
+    downloadBlob("playerquery", "PlayerData-000000000000", "PlayerData.csv")
 
-    #X,y = preprocessPlayerData()
+    X,y = preprocessPlayerData()
     #X,y = preprocessGamesData()
-    #X_test, y_test, y_pred, y_score = runLogisticRegression(X,y)
+    X_test, y_test, y_pred, y_score = runLogisticRegression(X,y)
     #X_test, y_test, y_pred, y_score = runSVM(X,y)
     #X_test, y_test, y_pred, y_score = runDecTree(X,y)
 
-    #getROC(X_test, y_test, y_pred, y_score)
+    getROC(X_test, y_test, y_pred, y_score)
     #getHeatMap(X_test, y_test, y_pred, y_score)
-    #getTSNE(X_test, y_test, y_pred, y_score)
-
-
+    getTSNE(X_test, y_test, y_pred, y_score)
 
 
 
@@ -46,7 +44,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
-
